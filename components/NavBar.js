57a60e7286eab156e2ee856e406810bbe1526a4a
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-08-21 11:20:49 -0400

    Created a navbar with routing to pages with placeholders

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import {
-  Navbar, Container, Nav, Button,
+  Navbar, Nav, Button, Image,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
-function CollapsibleExample() {
+function NavigationBar() {
   return (
-    <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">INDIE.TM</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/showForm">Add Show</Nav.Link>
-            <Nav.Link href="/showFullTour">View Full Tour</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="/viewProfile">View Profile</Nav.Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar style={{ paddingLeft: 20, paddingRight: 20 }} collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+      <Navbar.Brand href="/">
+        <Image src="/IndieLogo2.png" height={40} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/showForm">Add Show</Nav.Link>
+          <Nav.Link href="/showFullTour">View Full Tour</Nav.Link>
+        </Nav>
+        <Button variant="danger" onClick={signOut}>Sign Out</Button>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
 
-export default CollapsibleExample;
+export default NavigationBar;
