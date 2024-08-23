af448c9ce125ebf22f53176b3a7d2cec02c1f6d2
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-08-23 16:35:44 -0400

    Created a tour picker form that renders the tour names in the form

--- pages/showFullTour.js
+++ pages/showFullTour.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
+import { useState, useEffect, React } from 'react';
 import { useAuth } from '../utils/context/authContext';
 import DateCard from '../dateCard';
 import { getAllDates } from '../api/datesData';
+import TourPickerForm from '../components/TourPickerForm';
 
 function ShowFullTour() {
   const [dates, setDates] = useState([]);
@@ -30,26 +30,14 @@ function ShowFullTour() {
       }}
     >
       <h5 style={{ color: 'white', marginBottom: '20px' }}>Please select a tour below:</h5>
-      <Dropdown
-        style={{ marginBottom: '60px', border: 'white solid 1px', borderRadius: '7.5px' }}
-      >
-        <Dropdown.Toggle style={{ backgroundColor: '#252626' }} variant="secondary" id="dropdown-basic">
-          Select A Tour!
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Tour 1</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Tour 2</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Tour 3</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      <TourPickerForm />
       <h2 style={{ color: 'white', marginBottom: '20px', fontWeight: '100' }}>No Tour Selected</h2>
       <div
         style={{
           display: 'flex',
           flexWrap: 'wrap',
           justifyContent: 'start',
-          gap: '10px',
+          gap: '20px',
         }}
       >
         {dates.map((tourDates) => (
